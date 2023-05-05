3a87cc8ebf4b7a279de006ad461529a42e425d52
LORENZO MESSIAS LACERDA <lorenzo.messias2@gmail.com>
2023-05-04 22:33:00 -0300

    adicionei view

---
+++ iKnow/Views/Cliente/Form.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace iKnow.Views.Cliente
+{
+    public class ClienteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
