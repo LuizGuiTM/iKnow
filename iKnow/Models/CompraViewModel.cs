3d9363ae4d1463d4dbcdb6b4847a04dcf279d16b
LORENZO MESSIAS LACERDA <lorenzo.messias2@gmail.com>
2023-05-04 21:52:12 -0300

    resto dos viewmodel

--- iKnow/Models/CompraViewModel.cs
+++ iKnow/Models/CompraViewModel.cs
@@ -2,12 +2,11 @@
 
 namespace iKnow.Models
 {
-    public abstract class PadraoViewModel
+    public abstract class CompraViewModel: PadraoViewModel
     {
         public int IdCliente { get; set; }
 
         public double ValorTotal { get; set; }
-
         public DateTime DataCompra { get; set; }
     }
 }
--- iKnow/Models/ProdutoViewModel.cs
+++ iKnow/Models/ProdutoViewModel.cs
@@ -2,6 +2,10 @@
 {
     public partial class ProdutoViewModel: PadraoViewModel
     {
-        public string Descricao
+        public string Descricao { get; set; }
+
+        public double Preco { get; set; }
+
+        public int QtdDisponivel { get; set; }
     }
 }
