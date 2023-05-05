40ea20b0a2371b3aa8ad1bd8dc883932ca5a934b
LORENZO MESSIAS LACERDA <lorenzo.messias2@gmail.com>
2023-05-04 21:40:02 -0300

    viewmodel de verdade

---
+++ iKnow/Models/FuncionarioViewModel.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace iKnow.Models
+{
+    public partial class FuncionarioViewModel: PadraoViewModel
+    {
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+
+        public double Salario { get; set; }
+
+        public string Cargo { get; set; }
+
+        public DateTime DataNasc { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string Estado { get; set; }
+    }
+}
