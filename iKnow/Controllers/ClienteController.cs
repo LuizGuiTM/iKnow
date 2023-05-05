6225b9d5c6c3c81b2a8b0e928dcf2b6894aedb63
LORENZO MESSIAS LACERDA <lorenzo.messias2@gmail.com>
2023-05-04 22:41:07 -0300

    form view

---
+++ iKnow/Controllers/ClienteController.cs
@@ -0,0 +1,18 @@
+﻿using iKnow.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iKnow.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Form()
+        {
+            return View(new ClienteViewModel());
+        }
+    }
+}
