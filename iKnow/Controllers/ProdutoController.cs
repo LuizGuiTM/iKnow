50facbf9d22ecdeeaf816f9740eec70efc91575c
LuizGuiTM <l.guilhermedesouza@gmail.com>
2023-05-17 22:35:11 -0300

    tela de produto

---
+++ iKnow/Controllers/ProdutoController.cs
@@ -0,0 +1,56 @@
+﻿using iKnow.DAO;
+using iKnow.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.IO;
+
+namespace iKnow.Controllers
+{
+    public class ProdutoController : PadraoController<ProdutoViewModel>
+    {
+        public ProdutoController()
+        {
+            ExigeAutenticacao = false;
+            DAO = new ProdutoDAO();
+            GeraProximoId = true;
+        }
+        public byte[] ConvertImageToByte(IFormFile file)
+        {
+            if (file != null)
+                using (var ms = new MemoryStream())
+                {
+                    file.CopyTo(ms);
+                    return ms.ToArray();
+                }
+            else
+                return null;
+        }
+        protected override void ValidaDados(ProdutoViewModel model, string operacao)
+        {
+            base.ValidaDados(model, operacao);
+            if (string.IsNullOrEmpty(model.Nome))
+                ModelState.AddModelError("Nome", "Preencha o nome.");
+            //Imagem será obrigatio apenas na inclusão.
+            //Na alteração iremos considerar a que já estava salva.
+            if (model.Imagem == null && operacao == "I")
+                ModelState.AddModelError("Imagem", "Escolha uma imagem.");
+            if (model.Imagem != null && model.Imagem.Length / 1024 / 1024 >= 2)
+                ModelState.AddModelError("Imagem", "Imagem limitada a 2 mb.");
+            if (ModelState.IsValid)
+            {
+                //na alteração, se não foi informada a imagem, iremos manter a que já estava salva.
+                if (operacao == "A" && model.Imagem == null)
+                {
+                    ProdutoViewModel cid = DAO.Consulta(model.Id);
+                    model.ImagemEmByte = cid.ImagemEmByte;
+                }
+                else
+                {
+                    model.ImagemEmByte = ConvertImageToByte(model.Imagem);
+                }
+            }
+
+        }
+    }
+}
