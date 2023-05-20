5f3d73b8ffbdbde26942793c4b929b8b576164b0
caiomunicelli <caiomunicelli@gmail.com>
2023-05-20 17:10:58 -0300

    LoginCSS

--- iKnow/DAO/ConexaoBD.cs
+++ iKnow/DAO/ConexaoBD.cs
@@ -11,8 +11,9 @@ namespace iKnow.DAO
         public static SqlConnection GetConexao()
         {
             //string strCon = "Data Source=LOCALHOST; Database=iKnow; user id=sa; password=123456";
-            string strCon = @"Data Source=DESKTOP-MMNHGJS\SQLEXPRESS;Initial Catalog=iKnow;integrated security=true";
+            //string strCon = @"Data Source=DESKTOP-MMNHGJS\SQLEXPRESS;Initial Catalog=iKnow;integrated security=true";
             //string strCon = "Data Source=LG\\SQLEXPRESS;Initial Catalog=iKnow2;Trusted_connection=true;encrypt=false";
+            string strCon = "Data Source=LOCALHOST\\SQLEXPRESS; Database=iKnow; integrated security=true";
             SqlConnection conexao = new SqlConnection(strCon);
             conexao.Open();
             return conexao;
